c06a8c43b7b421fcc15f178156f2805445241beb
ElGato316 <chris.diaz24@me.com>
2021-04-07 20:34:08 -0400

    Fixed Javascript

--- application/views/users/view.php
+++ application/views/users/view.php
@@ -39,9 +39,6 @@
             </tbody>
         </table>
     </div><!-- Body of Document -->
-    <script src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
-    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <script>
         $(document).ready(function () {
             $('#users').DataTable({
@@ -51,12 +48,3 @@
             });
         });
     </script>
-
-<!--     <script>
-        $("document").ready(function(){
-            setTimeout(function(){
-                $('#flashdata').remove();
-            }, 5000 ); // 5 secs
-
-        });
-    </script> -->
